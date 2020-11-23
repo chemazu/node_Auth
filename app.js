b6f6105b18dc84fb36dcc0d81627a1cd3d6f2771
chemazu <chukwuemekachemazu@gmail.com>
2020-11-24 00:57:36 +0100

    register works

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ const db =require("./config/keys").mongoURI
 
 mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true}).then(()=>console.log("mongo Running")).catch(err=>{console.log(err)})
 
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/auth',register)
+// app.use(require('connect').bodyParser())
 app.listen(
     PORT,
     // console.log(`server running in ${process.env.NODE_ENV} mode in port ${P0RT}`)
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,30 @@
-const { Router } = require("express")
+// const { Router } = require("express")
+
 
 const router= require("express").Router()
+const User = require("../models/user.model")
+
+
 
 router.route('/register').get((req,res)=>{
+    // const {username,email,password}=req.body
     res.status(200).json('register')
+})
+
+router.route('/register').post((req,res)=>{
+    const {username,email,password}=req.body
+    // const username=req.body.username
+    // const email=req.body.email
+    // const password=req.body.password
+    const user = new User({
+        username,
+        email,
+        password
+    })
+    user.save().then(()=>{res.status(200).json('new user son')})
+    // res.status(200).json('registered')
+
+console.log(req.body)
 })
 
 module.exports = router; 
